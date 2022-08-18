d27e60df7d737d862f7a01cb8d494dfc7f3e8a90
NewtonOmbese <newtonombese1@gmail.com>
2022-08-18 11:33:09 +0300

    got it working

--- script.js
+++ script.js
@@ -3,11 +3,17 @@ const hr = document.getElementById("hr");
 const min = document.querySelector("#min");
 const sec = document.getElementById("sec");
 
-let day = new Date();
-// console.log(day);
-let hh = day.getHours() * 30;
-let mm = day.getMinutes() * deg;
-let ss = day.getSeconds() * deg;
+setInterval(()=>{
+    let day = new Date();
+    // console.log(day);
+    let hh = day.getHours() * 30;
+    let mm = day.getMinutes() * deg;
+    let ss = day.getSeconds() * deg;
+    
+    hr.style.transform = `rotateZ(${(hh) + (mm/12)}deg)`;
+    min.style.transform = `rotateZ(${mm}deg)`;
+    sec.style.transform = `rotateZ(${ss}deg)`;
+})
 
 
 
